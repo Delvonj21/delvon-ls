976cb91266752b877196fc0ccc9e7175bfd5d11b
Delvon <delvonj21@gmail.com>
2024-06-19 08:37:53 +0200

    added new files

---
+++ py101/lesson_2/mortgage_calculator.py
@@ -0,0 +1,8 @@
+# inputs:
+#     the loan amount
+#     the Annual percentage rate (APR)
+#     the loan duration
+
+# outputs:
+#     monthly interest rate (APR / 12)
+#     loan duration in months
