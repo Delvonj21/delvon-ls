eeca3b919b94636e7ffba07ce9f1423c45bcb1a6
Delvon <delvonj21@gmail.com>
2024-06-28 09:24:10 +0200

    added files

---
+++ py101/exercises/easy_1/utf_16.py
@@ -0,0 +1,11 @@
+# Write a function that determines and returns the UTF-16 string value of a string passed in as an argument. The UTF-16 string value is the sum of the UTF-16 values of every character in the string. (You may use ord to determine the UTF-16 value of a character.)
+
+# 1.	Define a function to take a single argument (the string).
+# 2.	Initialize a variable to keep track of the total UTF-16 value sum.
+# 3.	Iterate over each character in the string:
+# •	Convert the character to its UTF-16 value.
+# •	Add this value to the running total.
+# 4.	Return the total sum of UTF-16 values.
+
+
+
