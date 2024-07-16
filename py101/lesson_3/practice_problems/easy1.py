44229a18dcfdcb270a0cac13c3def6491d1d02b9
Delvon <delvonj21@gmail.com>
2024-07-16 10:55:07 +0200

    added files

---
+++ py101/lesson_3/practice_problems/easy1.py
@@ -0,0 +1,73 @@
+#1 Will the code below raise an error?
+
+# numbers = [1, 2, 3]
+# numbers[6] = 5
+
+# Yes, It will raise an IndexError
+
+#2 How can you determine whether a given string ends with an exclamation mark (!)? Write some code that prints True or False depending on whether the string ends with an exclamation mark.
+ 
+str1 = "Come over here!"  # True
+str2 = "What's up, Doc?"  # False
+
+print(str1.endwith("!"))
+print(str2.endswith("!"))
+
+#3 Starting with the string:
+
+famous_words = "seven years ago..."
+
+# Show two different ways to create a new string with "Four score and " prepended to the front of the string.
+
+new_string = f'Four score and {famous_words}'
+new_string = 'Four score and ' + famous_words
+
+#4 Using the following string, print a string that contains the same value, but using all lowercase letters except for the first character, which should be capitalized.
+
+munsters_description = "the Munsters are CREEPY and Spooky."
+# => 'The munsters are creepy and spooky.'
+
+print(munsters_description.capitalize())
+
+#5 Starting with the string:
+
+munsters_description = "The Munsters are creepy and spooky."
+
+# print the string with the case of all letters swapped:
+
+"tHE mUNSTERS ARE CREEPY AND SPOOKY."
+That is, lowercase letters are converted to uppercase, and uppercase letters are converted to lowercase"
+
+print(munsters_description.swapcase())
+
+#6 Determine whether the name Dino appears in the strings below -- check each string separately:
+
+str1 = "Few things in life are as important as house training your pet dinosaur."
+str2 = "Fred and Wilma have a pet dinosaur named Dino."
+
+print('Dino' in str1)
+print('Dino' in str2)
+
+#7 How can we add the family pet, "Dino", to the following list?
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"]
+
+flintstones.append('Dino')
+
+#8 How can we add multiple items to our list (e.g., 'Dino' and 'Hoppy')? Replace the call to append with another method invocation.
+
+flintstones.extend(['Dino', 'Hoppy'])
+
+#9 Print a new version of the sentence given by advice that ends just before the word house. Don't worry about spaces or punctuation: remove everything starting from the beginning of house to the end of the sentence.
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+# Expected output:
+# Few things in life are as important as
+
+print(advice.split('house')[0])
+
+#10 Print the following string with the word important replaced by urgent:
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice = advice.replace('important', 'urgent')
