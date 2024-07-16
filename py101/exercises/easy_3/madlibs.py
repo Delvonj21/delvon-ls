5faef672a4a465fa0a83e903c98c7c41f349e779
Delvon <delvonj21@gmail.com>
2024-07-16 10:21:30 +0200

    added files

---
+++ py101/exercises/easy_3/madlibs.py
@@ -0,0 +1,17 @@
+# Madlibs is a simple game where you create a story template with "blanks" for words. You, or another player, then construct a list of words and place them into the story, creating an often silly or funny story as a result.
+
+# Create a simple madlib program that prompts for a noun, a verb, an adverb, and an adjective, and injects them into a story that you create.
+
+# Prompt for a noun 
+noun = input('Enter a noun: ')
+# Prompt for a verb
+verb = input('Enter a verb: ')
+# Prompt for an adverb
+adverb = input('Enter an adverb: ')
+# Prompt for an adjective
+adjective = input('Enter an adjective: ')
+# Create a story
+# Inserts words into story
+print(f"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!")
+print(f"The {adjective} {noun} {verb} {adverb} over the lazy dog.")
+print(f"The {noun} {adverb} {verb} up to Joe's {adjective} turtle")
