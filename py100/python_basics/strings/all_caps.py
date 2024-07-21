cd4058e454fc40c7aeaad31813c8272d183a13a4
Delvon <delvonj21@gmail.com>
2024-07-21 11:09:12 +0200

    added files

---
+++ py100/python_basics/strings/all_caps.py
@@ -0,0 +1,5 @@
+# Convert the string 'confetti floating everywhere' to all upper case.
+
+string = 'confetti floating everywhere'
+cap_string = string.upper()
+print(cap_string)
---
+++ py100/python_basics/strings/length.py
@@ -0,0 +1,6 @@
+# Determine the length of the string "These aren't the droids you're looking for.".
+
+string = "These aren't the droids you're looking for."
+length_of_string = len(string)
+
+print(length_of_string)
